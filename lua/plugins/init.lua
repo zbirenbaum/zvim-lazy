0c95931694f3e8248777b813cd0bddb7f4e60175
zbirenbaum <zachary.birenbaum@wandb.com>
2026-02-09 15:42:22 -0800

    blink

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -185,13 +185,13 @@ local plugins = {
       }
     end,
   },
-  {
-    'zbirenbaum/copilot-cmp',
-    config = function ()
-      require('copilot_cmp').setup()
-    end,
-    enabled = true,
-  },
+  -- {
+  --   'zbirenbaum/copilot-cmp',
+  --   config = function ()
+  --     require('copilot_cmp').setup()
+  --   end,
+  --   enabled = true,
+  -- },
   -- {
   --   'zbirenbaum/neodim',
   --   event = {'LspAttach'},
@@ -199,19 +199,52 @@ local plugins = {
   --     require('neodim').setup()
   --   end
   -- },
+  -- {
+  --   'hrsh7th/nvim-cmp',
+  --   event = { 'InsertEnter', 'CursorHold' },
+  --   config = function()
+  --     require('plugins.completion.cmp_configs.cmp')
+  --   end,
+  --   dependencies = {
+  --     'folke/neodev.nvim',
+  --     'saadparwaiz1/cmp_luasnip',
+  --     'onsails/lspkind-nvim',
+  --     'hrsh7th/cmp-nvim-lsp',
+  --     'hrsh7th/cmp-path',
+  --   },
+  -- },
   {
-    'hrsh7th/nvim-cmp',
-    event = { 'InsertEnter', 'CursorHold' },
-    config = function()
-      require('plugins.completion.cmp_configs.cmp')
-    end,
-    dependencies = {
-      'folke/neodev.nvim',
-      'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind-nvim',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    build = "cargo build --release",
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+      completion = { documentation = { auto_show = false } },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" }
     },
+    opts_extend = { "sources.default" }
   },
   {
     'ray-x/lsp_signature.nvim',
